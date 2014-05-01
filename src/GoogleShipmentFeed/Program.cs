481b4b9ce7716f97d7eba8e245a6bf1259a65b80
Kyle Gobel <kgobel@deerso.com>
2014-05-01 12:34:19 -0400

    added logging

--- src/GoogleShipmentFeed/Program.cs
+++ src/GoogleShipmentFeed/Program.cs
@@ -2,34 +2,56 @@
 using System.Configuration;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Chronos;
 using Chronos.Configuration;
 using Data;
+using Deerso.Logging;
 using FeedLibrary;
 using Models;
+using ServiceStack.Logging;
 
 namespace GoogleShipmentFeed
 {
     static class Program
     {
+        private static ILog _log;
         static void Main()
         {
-            var now = DateTime.Now;
-            var lastRunTimestamp = ConfigUtils.GetAppSetting("lastRun", DateTime.Now.AddDays(-30).ToUnixTimestamp());
+            Init();
+            _log.Debug("App started");
 
-            var dataRepo = new Repo();
+            try
+            {
+                var now = DateTime.Now;
+                var lastRunTimestamp = ConfigUtils.GetAppSetting("lastRun", DateTime.Now.AddDays(-30).ToUnixTimestamp());
 
-            MakeShipmentFeed(dataRepo, lastRunTimestamp.FromUnixTimestamp(), now);
+                var dataRepo = new Repo();
 
-            MakeCancellationFeed(dataRepo, lastRunTimestamp.FromUnixTimestamp(), now);
+                MakeShipmentFeed(dataRepo, lastRunTimestamp.FromUnixTimestamp(), now);
 
-            UpdateLastRun(now);
+                MakeCancellationFeed(dataRepo, lastRunTimestamp.FromUnixTimestamp(), now);
 
+                UpdateLastRun(now);
+            }
+            catch (Exception e)
+            {
+               _log.Fatal("Error in Catch all", e); 
+                throw;
+            }
+            _log.Debug("App finished");
+        }
+
+        private static void Init()
+        {
+            LogManager.LogFactory = new DeersoLogFactory(debugEnabled:true, notifyNames: new [] {"@KyleGobel"});
+            _log = LogManager.GetLogger("");
         }
 
         private static void MakeCancellationFeed(Repo dataRepo, DateTime lastRunDate, DateTime startProgramTime)
         {
+            _log.Debug("Making cancellation feed");
             var feedData = dataRepo.GetCancelFeed(lastRunDate, startProgramTime);
 
             var feed = FeedMaker.GenerateFeed(FeedType.TSV, new FeedOptions {QualifyOptions = QualifyData.None},
@@ -45,10 +67,12 @@ namespace GoogleShipmentFeed
             }
 
             File.WriteAllText(feedPath, feed);
+            _log.DebugFormat("Finished making cancel feed with {0} items",feedData.Count());
         }
 
         static void MakeShipmentFeed(Repo repo, DateTime lastRunDate, DateTime startProgramDate)
         {
+            _log.Debug("Making shipment feed");
             var feedData = repo.GetShipmentFeed(lastRunDate, startProgramDate);
 
             var feed = FeedMaker.GenerateFeed(FeedType.TSV, new FeedOptions {QualifyOptions = QualifyData.None},
@@ -62,8 +86,7 @@ namespace GoogleShipmentFeed
                     Path.GetExtension(feedPath);
             }
             File.WriteAllText(feedPath, feed);
-
-
+            _log.DebugFormat("Completed shipment feed, added {0} item(s)",feedData.Count());
         }
 
         static void UpdateLastRun(DateTime date)
